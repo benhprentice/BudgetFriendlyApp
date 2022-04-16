52ddaf0054f20ce680a825b968a1c5997975ba02
benhprentice <jabberthok@yahoo.com>
2022-04-15 22:16:54 -0500

    Clean up mainActivity

--- BudgetFriendlyApp/app/src/main/java/com/example/budgetfriendlyapp/MainActivity.java
+++ BudgetFriendlyApp/app/src/main/java/com/example/budgetfriendlyapp/MainActivity.java
@@ -40,15 +40,11 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         //comment if statement to bring back login page
-<<<<<<< Updated upstream
-        if (mAuth.getCurrentUser() != null) {
-            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-        }
-=======
-        /*if(mAuth.getCurrentUser()!=null){
-            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
-        }*/
->>>>>>> Stashed changes
+
+//        if (mAuth.getCurrentUser() != null) {
+//            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+//        }
+
 
         mDialog = new ProgressDialog(this);
 
